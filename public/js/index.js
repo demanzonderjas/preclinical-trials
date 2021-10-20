58f64a3e371804c54c67152996bea18bb6277008
Daan Weustenraad <weustenraad@gmail.com>
2021-10-20 16:27:45 +0200

    add footer

--- public/js/index.js
+++ public/js/index.js
@@ -39463,8 +39463,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PageFooter = void 0;
 var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+var Image_1 = __webpack_require__(/*! ../base/Image */ "./resources/js/components/base/Image.tsx");
 exports.PageFooter = function () {
-    return react_1.default.createElement("div", { className: "PageFooter" }, "Footer");
+    return (react_1.default.createElement("div", { className: "PageFooter" },
+        react_1.default.createElement("div", { className: "links" },
+            react_1.default.createElement("ul", null,
+                react_1.default.createElement("li", null, "Disclaimer"),
+                react_1.default.createElement("li", null, "Contact"),
+                react_1.default.createElement("li", null,
+                    react_1.default.createElement(Image_1.Image, { filename: "twitter.png" }))))));
 };
 
 
--- resources/js/components/layout/PageFooter.tsx
+++ resources/js/components/layout/PageFooter.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Image } from "../base/Image";
 
 export const PageFooter: React.FC = () => {
-	return <div className="PageFooter">Footer</div>;
+	return (
+		<div className="PageFooter">
+			<div className="links">
+				<ul>
+					<li>Disclaimer</li>
+					<li>Contact</li>
+					<li>
+						<Image filename="twitter.png" />
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 };
