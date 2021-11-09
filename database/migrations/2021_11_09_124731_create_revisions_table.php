fefe581618f34a4ff024e2674cfbe6a323275015
Daan Weustenraad <weustenraad@gmail.com>
2021-11-09 13:57:19 +0100

    WIP add base models & migrations

---
+++ database/migrations/2021_11_09_124731_create_revisions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRevisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('revisions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('protocol_id')->unsigned();
+            $table->foreign('protocol_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('changes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('revisions');
+    }
+}
