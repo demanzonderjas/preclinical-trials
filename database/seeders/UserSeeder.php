3a48ecb36d115f4b8eae755d241b0a78d97bc79b
Daan Weustenraad <weustenraad@gmail.com>
2021-11-22 15:07:18 +0100

    add database migrations

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$user = new User();
+		$user->first_name =  "Daan";
+		$user->last_name =  "Weustenraad";
+		$user->password = Hash::make('appeltaart');
+		$user->email = "daan@puzzel.org";
+		$user->institution = "UU";
+		$user->save();
+	}
+}
