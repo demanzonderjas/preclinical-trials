d6b27e2a32794a037e56c429aaa10487d8967f93
Daan Weustenraad <weustenraad@gmail.com>
2021-11-25 16:00:58 +0100

    add drop filter

--- resources/js/components/cards/CardsBlock.tsx
+++ resources/js/components/cards/CardsBlock.tsx
@@ -1,13 +1,23 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
+import { useFilter } from "../../hooks/useFilter";
 import { TProtocol } from "../../typings/protocols";
+import { protocolMeetsFilters } from "../../utils/filters";
 import { ProtocolCard } from "./ProtocolCard";
 
-export const ProtocolCardsBlock: React.FC<{ protocols: TProtocol[] }> = ({ protocols }) => {
-	return (
-		<div className="CardsBlock">
-			{protocols.map(p => (
-				<ProtocolCard key={p.id} protocol={p} />
-			))}
-		</div>
-	);
-};
+export const ProtocolCardsBlock: React.FC<{ protocols: TProtocol[] }> = observer(
+	({ protocols }) => {
+		const { activeFilterText, activeFilterKey, filters } = useFilter();
+		return (
+			<div className="CardsBlock">
+				{protocols
+					.filter(row =>
+						protocolMeetsFilters(activeFilterText, activeFilterKey, filters, row)
+					)
+					.map(p => (
+						<ProtocolCard key={p.id} protocol={p} />
+					))}
+			</div>
+		);
+	}
+);
