a928e337a9e370eacd786641b5a970f526a4d00f
Daan Weustenraad <weustenraad@gmail.com>
2021-12-02 14:03:32 +0100

    fix success text

--- resources/js/components/forms/SuccessMessage.tsx
+++ resources/js/components/forms/SuccessMessage.tsx
@@ -1,13 +1,15 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { useForm } from "../../hooks/useForm";
+import { useTranslationStore } from "../../hooks/useTranslationStore";
 
 export const SuccessMessage: React.FC = observer(() => {
 	const { succesText, isDone } = useForm();
+	const { t } = useTranslationStore();
 
 	if (!succesText || !isDone) {
 		return null;
 	}
 
-	return <div className="SuccessMessage">{succesText}</div>;
+	return <div className="SuccessMessage">{t(succesText)}</div>;
 });
