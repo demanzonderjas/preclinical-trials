ee83e8e6a86fdf5004ed494ebed5921749565693
Daan Weustenraad <weustenraad@gmail.com>
2021-12-07 17:21:05 +0100

    add highligjt action

--- resources/js/components/layout/Highlight.tsx
+++ resources/js/components/layout/Highlight.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router";
 import { Image } from "../base/Image";
 
 export const Highlight: React.FC<{ text: string; image: string }> = ({ text, image }) => {
+	const { push } = useHistory();
 	return (
-		<div className="Highlight">
+		<div className="Highlight" onClick={() => push("/database")}>
 			<div className="copy">
 				<p>{text}</p>
 				<Image filename={image} />
