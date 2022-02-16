01a067aa8c3fa018427de2073a90bc107fb04489
Daan Weustenraad <weustenraad@gmail.com>
2022-02-16 10:39:54 +0100

    add better contact button

--- resources/js/pages/Contact.tsx
+++ resources/js/pages/Contact.tsx
@@ -18,10 +18,24 @@ export const ContactPage: React.FC = () => {
 				<ContentBlock withBorder>
 					<div
 						className="layout-wrapper"
-						style={{ display: "flex", justifyContent: "center" }}
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							flexWrap: "wrap",
+							background: "#f0f0f0",
+							padding: "20px"
+						}}
 					>
+						<p style={{ width: "100%", textAlign: "center" }}>
+							{t("click_button_to_set_up_an_appointment")}
+						</p>
 						<button
 							type="button"
+							className="tertiary"
+							style={{
+								filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.65)",
+								border: "1px solid rgba(0, 0, 0, 0.65)"
+							}}
 							onClick={() =>
 								window.open(
 									"https://preclinicaltrialseu.simplybook.it/v2/#book",
