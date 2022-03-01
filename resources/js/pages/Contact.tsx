b352df0f3acdda8c4a10c8d8506c2365cd6dd8ae
Daan Weustenraad <weustenraad@gmail.com>
2022-03-01 18:24:38 +0100

    move appointment button

--- resources/js/pages/Contact.tsx
+++ resources/js/pages/Contact.tsx
@@ -17,45 +17,14 @@ export const ContactPage: React.FC = () => {
 			<div className="ContactPage">
 				<ContentBlock withBorder>
 					<div
-						className="layout-wrapper"
-						style={{
-							display: "flex",
-							justifyContent: "center",
-							flexWrap: "wrap",
-							background: "#f0f0f0",
-							padding: "20px"
-						}}
-					>
-						<p style={{ width: "100%", textAlign: "center" }}>
-							{t("click_button_to_set_up_an_appointment")}
-						</p>
-						<button
-							type="button"
-							className="tertiary"
-							style={{
-								filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.65)",
-								border: "1px solid rgba(0, 0, 0, 0.65)"
-							}}
-							onClick={() =>
-								window.open(
-									"https://preclinicaltrialseu.simplybook.it/v2/#book",
-									"_blank",
-									"noreferrer noopener"
-								)
-							}
-						>
-							{t("set_up_an_appointment")}
-						</button>
-					</div>
+						dangerouslySetInnerHTML={{ __html: page.content_blocks[0].content }}
+						style={{ textAlign: "center" }}
+					/>
 					<FormBlock
 						icon="note.png"
 						form={contactForm}
 						handleSubmit={saveContactFormQuery}
 					></FormBlock>
-					<div
-						dangerouslySetInnerHTML={{ __html: page.content_blocks[0].content }}
-						style={{ textAlign: "center" }}
-					/>
 					<div className="social-icons">
 						<SocialIcons />
 					</div>
--- resources/js/pages/Help.tsx
+++ resources/js/pages/Help.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { AppointmentButton } from "../components/layout/AppointmentButton";
 import { ExpandableText } from "../components/layout/ExpandableText";
 import { PrimaryHeaderPageWithSubMenu, PageWithSubmenu } from "../components/layout/Page";
 import { usePage } from "../hooks/usePage";
+import { useTranslationStore } from "../hooks/useTranslationStore";
 import { getFAQ } from "../queries/faq";
 import { TFaqCategory, TPublishStatus } from "../typings/faq";
 import { TSubMenu } from "../typings/layout";
@@ -10,6 +12,7 @@ export const HelpPage: React.FC = () => {
 	const [faqCategories, setFaqCategories] = useState<TFaqCategory[]>([]);
 	const [activeCategory, setActiveCategory] = useState<TFaqCategory>(null);
 	const { page } = usePage();
+	const { t } = useTranslationStore();
 
 	useEffect(() => {
 		(async () => {
@@ -28,6 +31,10 @@ export const HelpPage: React.FC = () => {
 	return (
 		<PrimaryHeaderPageWithSubMenu title={page.title}>
 			<PageWithSubmenu subMenu={subMenu}>
+				<p
+					dangerouslySetInnerHTML={{ __html: t("help_intro") }}
+					style={{ marginBottom: "40px" }}
+				/>
 				<div className="HelpPage">
 					{activeCategory?.faq_items
 						.filter(item => item.status !== TPublishStatus.Draft)
