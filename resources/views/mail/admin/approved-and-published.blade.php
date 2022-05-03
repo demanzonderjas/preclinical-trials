20b1fe21813a66487968ebffc7b8b3c62b830ac5
Daan Weustenraad <weustenraad@gmail.com>
2022-05-03 13:28:18 +0200

    add approved published line

--- resources/views/mail/admin/approved-and-published.blade.php
+++ resources/views/mail/admin/approved-and-published.blade.php
@@ -7,6 +7,13 @@
 <p>
     Your protocol has been approved and published!
 </p>
+<p>
+    Your protocol ID is <strong>{{ $protocol->id }}</strong>.<br />
+    Your direct protocol URL is: <strong><a href="{{ env('APP_URL') . '/database/view-protocol/' . $protocol->id }}">{{ env('APP_URL') . '/database/view-protocol/' . $protocol->id }}</a></strong><br />
+</p>
+<p>
+    You can use this ID or the URL of your protocol to refer to your preregistration to your funder and in future report/publication
+</p>
 @stop
 
 @section('cta-link', env('APP_URL') . '/database/view-protocol/' . $protocol->id)
