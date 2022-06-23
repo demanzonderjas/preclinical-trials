7b55ed0ddca17ab37788504a606dd83f20e51104
Daan Weustenraad <weustenraad@gmail.com>
2022-06-23 09:59:01 +0200

    add encryption command

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -3,16 +3,29 @@
 namespace App\Models;
 
 use App\Casts\Json;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Detail extends Model
 {
     use HasFactory;
 
-    protected $casts = [
-        'value' => Json::class
-    ];
     protected $fillable = ["protocol_id", "key", "value"];
     protected $hidden = ["protocol_id", "id", "created_at", "updated_at"];
+
+    public function setValueAttribute($value)
+    {
+        $this->attributes['value'] = Crypt::encryptString(json_encode($value));
+    }
+
+    public function getValueAttribute($value)
+    {
+        try {
+            return json_decode(Crypt::decryptString($value));
+        } catch (DecryptException $e) {
+            return json_decode($value);
+        }
+    }
 }
