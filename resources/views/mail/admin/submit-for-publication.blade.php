1a50461f0f2c6c6ec335edc0a469f66b98198548
Daan Weustenraad <weustenraad@gmail.com>
2022-10-06 09:35:48 +0200

    add admin name

--- resources/views/mail/admin/submit-for-publication.blade.php
+++ resources/views/mail/admin/submit-for-publication.blade.php
@@ -2,19 +2,21 @@
 
 @section('title', 'PCT.eu - A new protocol has been submitted for publication!')
 @section('preheader', 'A new protocol has been submitted for publication.')
+@section('name', 'PCT Admin')
 
 @section('content')
-<p>
-    A{{ $protocol->status === "submitted_for_publication " ? " new" : " " }}protocol has been {{ $protocol->status === "submitted_for_publication" ? "submitted" : "resubmitted" }} for publication.
-</p>
+    <p>
+        A{{ $protocol->status === 'submitted_for_publication ' ? ' new' : ' ' }}protocol has been
+        {{ $protocol->status === 'submitted_for_publication' ? 'submitted' : 'resubmitted' }} for publication.
+    </p>
 @stop
 
 @section('cta-link', env('APP_URL') . '/admin/protocols/' . $protocol->id)
 @section('cta-text', 'View the protocol')
 
 @section('closing')
-<p>
-    Friendly regards,<br />
-    PCT.eu
-</p>
+    <p>
+        Friendly regards,<br />
+        PCT.eu
+    </p>
 @stop
