37e32ece6fcc7bb4fb2cc434e2e486df416ee76d
Daan Weustenraad <weustenraad@gmail.com>
2022-10-24 10:12:05 +0200

    add message db

---
+++ database/migrations/2022_10_24_080452_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('question_user_id')->unsigned();
+            $table->foreign('question_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->bigInteger('protocol_user_id')->unsigned();
+            $table->foreign('protocol_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('text')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
