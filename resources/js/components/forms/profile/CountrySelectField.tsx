a63ff54f340ed5c611693c09c1206f26ce144570
Daan Weustenraad <weustenraad@gmail.com>
2023-06-29 13:06:37 +0200

    add auto-loading

---
+++ resources/js/components/forms/profile/CountrySelectField.tsx
@@ -0,0 +1,24 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { getCountrySelectOptions } from "../../../utils/countries";
+import { useFormField } from "../../../hooks/useForm";
+import { TFormFieldName } from "../../../typings/forms";
+
+export const CountrySelectField: React.FC<{ id: TFormFieldName }> = observer(({ id }) => {
+	const { value, setValue } = useFormField(id);
+
+	return (
+		<select
+			value={value}
+			onChange={e => setValue(e.target.value)}
+			style={{ fontSize: "18px", padding: "10px" }}
+		>
+			<option value="" />
+			{getCountrySelectOptions().map(country => (
+				<option key={country.value} value={country.value}>
+					{country.text}
+				</option>
+			))}
+		</select>
+	);
+});
--- resources/js/components/tables/CountryCell.tsx
+++ resources/js/components/tables/CountryCell.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useTranslationStore } from "../../hooks/useTranslationStore";
 import { getCountries } from "../../utils/countries";
 
 export const CountryCell: React.FC<{ value: string }> = ({ value }) => {
