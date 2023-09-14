0e9be52651fca0f8deace894cdef43c1e44fd906
Daan Weustenraad <weustenraad@gmail.com>
2023-09-14 09:27:11 +0200

    fix title

--- resources/views/mail/layout.blade.php
+++ resources/views/mail/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="viewport" content="width=device-width">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>ATEX - @yield('title')</title>
+    <title>PCT - @yield('title')</title>
     <style>
         @media only screen and (max-width: 620px) {
             table[class=body] h1 {
